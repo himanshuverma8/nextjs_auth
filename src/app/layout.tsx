f694cdb555e846a2bb8614b54cb93e18c63fc000
Himanshu Verma <hvinprimary@gmail.com>
2025-05-14 21:29:23 +0530

    add session provider from nextauth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+"use client"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,11 +13,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "authentication",
-  description: "created by hv",
-};
 
 export default function RootLayout({
   children,
@@ -28,7 +25,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <link rel="icon" href="https://files.hvin.tech/lighting_logo.png" sizes="any" />
-        {children}
+       <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
   );
